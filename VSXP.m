afdcfc49c3a2fa6cc383675f9e34d6f1cc5e2fac
thorstone25 <tbrevett@gmail.com>
2023-07-21 18:26:16 -0700

    Removing unnecessary files.

--- VSXP.m
+++
@@ -1,9 +0,0 @@
-classdef VSXP < matlab.mixin.Copyable
-    properties
-        startDepth (1,1) double
-        endDepth (1,1) double
-        numTx (1,1) double
-        numRays (1,1) double
-        txFocus (1,1) double
-    end
-end
