ebfc1ed5d5e6c62e5503ac64a18b4a2980bd22b0
thorstone25 <tbrevett@gmail.com>
2023-07-22 00:50:31 -0700

    VSXRcvBuffer() : moved argument validator helper function.

--- VSXRcvBuffer.m
+++ VSXRcvBuffer.m
@@ -5,16 +5,18 @@ classdef VSXRcvBuffer < matlab.mixin.Copyable
         colsPerFrame (1,1) double = 128
         numFrames (1,1) double {mustBe1OrEven} = 1
     end
-    
+
     methods
         function obj = VSXRcvBuffer(kwargs)
             arguments, kwargs.?VSXRcvBuffer, end % arguments
             for f = string(fieldnames(kwargs))', obj.(f) = kwargs.(f); end % init
-
-            value = obj.numFrames;
-            if ~(isequal(value, 1) || rem(value, 2) == 0)
-                error('Property value must be 1 or an even number.');
-            end     
         end
     end
+end
+
+% validator(s)
+function mustBe1OrEven(value)
+if ~(isequal(value, 1) || rem(value, 2) == 0)
+    error('NumFrames must be 1 or an even number.');
+end
 end
