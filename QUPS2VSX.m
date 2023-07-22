0251f6f2ecec63a0bf469ed117d512aa3c4ff5d7
thorstone25 <tbrevett@gmail.com>
2023-07-21 19:57:18 -0700

    QUPS2VSX() : modified function signature; reorganized code; removed some hard-coded constants.

--- QUPS2VSX.m
+++ QUPS2VSX.m
@@ -1,27 +1,49 @@
-function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process, vSeqControl, vTGC, vReconInfo, t0] = QUPS2VSX(us, vResource, xdc_name, kwargs)
+function [vBlock, vPData, vTrans, vUI, t0, vTW, vTX, vRcv, vRecon, display_image_process, vSeqControl, vTGC, vReconInfo] = QUPS2VSX(us, xdc, vResource, kwargs)
+% QUPS2VSX - Verasonics structure converter
+%
+% [vBlock, vPData, vTrans] = QUPS2VSX(us) converts the UltrasoundSystem us
+% into a VSXBlock vBlock, with a default VSXPData vPData and a Trans
+% structure vTrans. These can be used with VSXBlock.link to generate
+% Verasonics compatible configuration structure.
+%
+% [...] = QUPS2VSX(us, xdc) where xdc is a string uses the named transducer
+% xdc and the `computeTrans` utility to generate the transducer struct. 
+% If xdc is a struct, it is assumed to be the verasonics Trans struct and
+% the `computeTrans` utility is used to complete the definition.
+% If xdc is empty, a custom transducer definition is generated. The default
+% is string.emtpy.
+%
+% [...] = QUPS2VSX(us, xdc_name, vResource) uses the VSXResource vResource
+% instead of creating a new VSXResource. You must specify this if 
     arguments
         us (1,1) UltrasoundSystem
-        vResource (1,1) VSXResource
-        xdc_name (1,1) string % transducer name
+        xdc {mustBeScalarOrEmpty, mustBeA(xdc, ["string", "struct"])} = string.empty % transducer name
+        vResource (1,1) VSXResource = VSXResource()
         kwargs.units (1,1) string {mustBeMember(kwargs.units, ["mm", "wavelengths"])} = "mm"
         kwargs.vTW (1,1) VSXTW = VSXTW('type', 'parametric', 'Parameters', [us.xdc.fc/1e6, 0.67, 1, 1]); %-
         kwargs.vTGC (1,1) VSXTGC = VSXTGC('CntrlPts', [0,297,424,515,627,764,871,1000],...
                                           'rangeMax', hypot(us.scan.zb(2), us.scan.xb(2)) ./ us.lambda); %-
+        kwargs.frames (1,1) {mustBeInteger, mustBePositive} = 1;
     end
 
     % squash obj to struct warning
     warning_state = warning('off', 'MATLAB:structOnObject');
     
-    %% create a scan from us.scan
-    scan = us.scan; % should be ScanCartesian
-    assert(isa(scan, 'ScanCartesian'));
-    
+%% Handle inputs
+F = kwargs.frames;
+
         %% Trans
-    if isscalar(xdc_name)
-            vTrans.name = char(xdc_name);
+    if isscalar(xdc)
+        if isa(xdc, 'string') % interpret as name
+            vTrans.name = char(xdc);
             vTrans.units = char(kwargs.units);
-            vTrans = computeTrans(vTrans);
-    else
+        elseif isa(xdc, 'struct') % interpret as Trans
+            vTrans = xdc; 
+        else
+            error("Unrecognized input for xdc.");
+        end
+    elseif isempty(xdc) % make custom
+        warning("Untested.");
         vTrans.spacing          = us.xdc.pitch;
         vTrans.Bandwidth        = us.xdc.bw;
         vTrans.elementWidth     = us.xdc.width;
@@ -29,116 +51,110 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
         vTrans.numelements      = us.xdc.numel;
         vTrans.frequency        = us.xdc.fc; 
         vTrans.elevationFocusMm = us.xdc.el_focus;
-
+    else
+        error("Unrecognized input for xdc.")
     end
-    
-    fs_decim = 20.8333; % decimation frequency - this is the supported frequency that verasonics will choose based on the ...
-    ...transducer frequency, sample mode, and frequency support list see the Vantage 4.3.0 programming manual, page 106
-    c0 = vResource.Parameters.speedOfSound; % I think?
+
+    % compute structure
+    vTrans = computeTrans(vTrans);
+
+    % set global params
+    c0 = us.seq.c0;
     lambda = c0 / (1e6*vTrans.frequency); % wavelengths
     
-    %% ---------------------------- Sequence params -------------------------- %
-    % Parameters for Focused Transmit Imaging Sequence
-    P.startDepth = 2;   %            acquisition start (wavelengths)
-    P.endDepth = 256;   % (requested) acquisition end (wavelengths)
-    P.numTx = 26;       % Number of active transmit elements in the aperture.
-    P.numRays = 192;    % Number of ray lines in frame.
-    P.txFocus = 130;    % transmit focal pt in wavelengths
-        
-    % set the maximum acquisition depth
-    maxAcqLength = ceil(sqrt(P.endDepth^2 + ((vTrans.numelements-1) * vTrans.spacing)^2));
-    spw = fs_decim / vTrans.frequency; % samples per wave
-    bufLen = 256 * ceil((maxAcqLength - P.startDepth) * spw / 256); % only modulus 256 sample buffer length supported
-    
-    %% Allocate buffers and Set Parameters
-    toggle = true;
-    F = 1; % number of frames
-    T = 256 * 20; %% bufLen;
-    
-    na = 7; %%us.seq.numPulse; % Set na = number of angles.
-    if (na > 1)
-        dtheta = (36*pi/180)/(na-1);
-        startAngle = -36*pi/180/2;
-    else
-        dtheta = 0;
-        startAngle=0;
-    end % set dtheta to range over +/- 18 degrees.
-
-    vResource.Parameters.speedOfSound = us.seq.c0; 
-    vResource.Parameters.verbose = 2; %%
-    vResource.RcvBuffer(end+1)   = VSXRcvBuffer('rowsPerFrame', T * us.seq.numPulse,...  %-
-                                                'colsPerFrame', vResource.Parameters.numRcvChannels,...
-                                                'numFrames', F);
-    vResource.InterBuffer(end+1) = VSXInterBuffer('numFrames', F);
-    vResource.ImageBuffer(end+1) = VSXImageBuffer('numFrames', F);
-  
-    %rectangular aperture only
-    
-   %% PData
+    % get the scan region in units of wavelengths so we know the ROI
+    assert(isa(us.scan, 'ScanCartesian')); % should be ScanCartesian
     scan = scale(us.scan, 'dist', 1./lambda);
+    
+    % get temporal sampling region
+    dnear = 2 * scan.zb(1); % nearest distance (2-way)
+    dfar  = 2 * hypot(range(scan.xb), scan.zb(end)); % furthest distance (2-way)
+    
+    %% PData
     vPData = VSXPData();
-%     vPData.PDelta = [0.5, 0, 0.5]; 
+    % vPData.PDelta = [0.5, 0, 0.5]; 
     vPData.PDelta = [scan.dx, 0, scan.dz]; 
     vPData.Size(1) = scan.nz; %-
     vPData.Size(2) = scan.nx; %-
     vPData.Size(3) = scan.ny; %-
     vPData.Origin = [scan.xb(1), scan.yb(1), scan.zb(1)];
     
-% %     vPData.Region = computeRegions(struct(vPData));
+    % TODO: compute pixel regions
+    % vPData.Region = computeRegions(struct(vPData));
 
-% %     vResource.DisplayWindow(end+1) = VSXDisplayWindow('ReferencePt', vPData.Origin);
-    vResource.DisplayWindow(1).Title = 'L11-5vFlashAngles';
-    vResource.DisplayWindow(1).pdelta = scan.dx; % 0.35;
-%     vResource.DisplayWindow(1).Position = [250,89.500000000000000,542,535];
-    vResource.DisplayWindow(1).Position = [250,89.500000000000000,scan.nx,scan.nz];
-    vResource.DisplayWindow(1).ReferencePt = [vPData(1).Origin(1),0,vPData(1).Origin(3)];   % 2D imaging is in the X,Z plane
-    vResource.DisplayWindow(1).numFrames = F;
-    vResource.DisplayWindow(1).AxesUnits = 'mm';
-    vResource.DisplayWindow(1).Colormap = gray(256);
+    % %     vResource.DisplayWindow(end+1) = VSXDisplayWindow('ReferencePt', vPData.Origin);
+    vDisplayWindow = VSXDisplayWindow('ReferencePt', vPData.Origin);
+    vDisplayWindow.Title = 'L11-5vFlashAngles';
+    vDisplayWindow.pdelta = scan.dx; % 0.35;
+    vDisplayWindow.Position = [250, 89.5, scan.nx, scan.nz];
+    vDisplayWindow.ReferencePt = [vPData(1).Origin(1),0,vPData(1).Origin(3)];   % 2D imaging is in the X,Z plane
+    vDisplayWindow.numFrames = F;
+    vDisplayWindow.AxesUnits = 'mm';
+    vDisplayWindow.Colormap = gray(256);
+    vResource.DisplayWindow(end+1) = vDisplayWindow;
 
+    %% Allocate buffers and Set Parameters
+
+    % infer the selected sampling frequency for NS200BW
+    fs_available = 250 ./ (100:-1:4); % all supported sampling frequencies
+    fs_decim = fs_available(find(fs_available >= 4 * vTrans.frequency, 1, 'first')); % decimation frequency for NS200BW    
+    
+    % get the output data buffer length
+    spw = fs_decim / vTrans.frequency; % samples per wave
+    bufLen = 256 * ceil((dfar - dnear) * spw / 256); % only modulus 256 sample buffer length supported
+    
+    % T = 256 * 20; %% bufLen;
+    T = bufLen;
+    
+    % make new buffers
+    vResource.RcvBuffer(end+1)   = VSXRcvBuffer('rowsPerFrame', T * us.seq.numPulse,...  %-
+                                                'colsPerFrame', vResource.Parameters.numRcvChannels,...
+                                                'numFrames', F);
+    vResource.InterBuffer(end+1) = VSXInterBuffer('numFrames', F);
+    vResource.ImageBuffer(end+1) = VSXImageBuffer('numFrames', F);
+    
+    % set other resource params
+    vResource.Parameters.speedOfSound = c0; 
+    vResource.Parameters.verbose = 2; % default verbosity
+
+      
      %% TW
     vTW = kwargs.vTW;
 
     %% TX
     vTX.Origin = [0.0,0.0,0.0]; %%
-    vTX.focus = 0.0; %%
-    vTX.Delay = zeros(1,vTrans.numelements); %%
-    vTX = repmat(VSXTX(), [1, us.seq.numPulse]);
-    vTX = sort(copy(vTX)); 
+    vTX = copy(repmat(VSXTX(), [1, us.seq.numPulse]));
     [vTX.waveform] = deal(vTW);
     
-    % - Set event specific TX attributes.
-    if fix(na/2) == na/2       % if na even
-        P.startAngle = (-(fix(na/2) - 1) - 0.5)*dtheta;
-    else
-        P.startAngle = -fix(na/2)*dtheta;
-    end
-    
+    % get delay and apodization matrices
     delay  = - us.seq.delays(us.xdc) * us.xdc.fc;
     apod = us.seq.apodization(us.xdc);
     t0 = min(delay, [], 1); % min over elements
     delay = delay - t0;
-    for i = 1:us.seq.numPulse
-        if us.seq.type == "VS"
-            vTX(i).FocalPt = us.seq.focus(:,i)/lambda;
-%             vTX(i).Origin(1) = us.seq.focus(1,i)/lambda;
-%             vTX(i).focus     = us.seq.focus(3,i)/lambda;
-        elseif us.seq.type == "PW"
-            vTX(i).Steer = deg2rad([us.seq.angles(i), 0]);
-        elseif us.seq.type == "FSA"
-            %do nothing
-        end
-        for j = 1:us.tx.numel
-            vTX(i).Apod(j) = apod(j,i);
-            vTX(i).Delay(j) = delay(j,i); 
-        end
-    end
-    for n = 1:na   % na transmit events
-        vTX(n).Steer = [(P.startAngle+(n-1)*dtheta),0.0];
-%         vTX(n).Delay = computeTXDelays(struct(vTX(n))); % requires base
-        % workspace variables
-    end
     
+    % - Set event specific TX attributes.
+    for i = 1:us.seq.numPulse
+        % set beamforming geometry
+        switch us.seq.type
+            case "VS"
+                vTX(i).FocalPt = us.seq.focus(:,i)/lambda;
+                % vTX(i).Origin(1) = us.seq.focus(1,i)/lambda;
+                % vTX(i).focus     = us.seq.focus(3,i)/lambda;
+            case "PW"
+                vTX(i).Steer = deg2rad([us.seq.angles(i), 0]);
+            case "FSA"
+                %do nothing
+        end
+
+        % set delays and apodization
+        vTX(i).Apod  =  apod(:,i)';
+        vTX(i).Delay = delay(:,i)';
+
+        % TODO: use computeTXDelays instead
+        % vTX(i).Delay = computeTXDelays(struct(vTX(i))); % requires base
+    end
+
+    % TODO: hack compute TXPD 
     for i = 1:us.seq.numPulse
 %         vTX(i).TXPD = computeTXPD(struct(vTX(i)), struct(vPData));
     end
@@ -148,10 +164,6 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
     vTGC.Waveform = computeTGCWaveform(vTGC, 1e6*vTrans.frequency);
 
     %% Rcv 
-    % get temporal sampling region
-    dnear = 2 * scan.zb(1);   
-    dfar  = 2 * hypot(range(scan.xb), scan.zb(end));    
-    
     vRcv = VSXReceive();
 %     vRcv.aperture = 1;
     vRcv.Apod = ones(1, [vResource.Parameters.numRcvChannels]);
@@ -163,13 +175,12 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
     vRcv.acqNum = 1; 
     vRcv.callMediaFunc = 0;
     
-    vRcv = repmat(vRcv,1,us.seq.numPulse);
-    vRcv = sort(copy(vRcv));
+    vRcv = copy(repmat(vRcv,1,us.seq.numPulse));
     
     vRcv(1).callMediaFunc = 1; %%
     % - Set event specific Receive attributes.
     for i = 1:vResource.RcvBuffer(1).numFrames 
-        for j = 1:na
+        for j = 1:us.seq.numPulse
             vRcv(j).framenum = i;
             vRcv(j).acqNum = j;
         end
@@ -180,24 +191,24 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
     vRecon.senscutoff = 0.6;
     vRecon.pdatanum = length(vPData);
     vRecon.rcvBufFrame = -1;
-    vRecon.IntBufDest = [1,1];
+    vRecon.IntBufDest = [1, 1];
     vRecon.ImgBufDest = [1,-1];
     
     % Define ReconInfo structures.
     % We need na ReconInfo structures for na steering angles.
-    vReconInfo = repmat(VSXReconInfo('mode', 'accumIQ', ...  % default is to accumulate IQ data.
+    vReconInfo = copy(repmat(VSXReconInfo('mode', 'accumIQ', ...  % default is to accumulate IQ data.
                        'txnum', 1, ...
                        'rcvnum', 1, ...
-                       'regionnum', 1), 1, na);
-    vReconInfo = sort(copy(vReconInfo));
+                       'regionnum', 1), ...
+                       [1, us.seq.numPulse]));
     % - Set specific ReconInfo attributes.
-    if na>1
+    if us.seq.numPulse > 1
         vReconInfo(1).mode = 'replaceIQ'; % replace IQ data
-        for j = 1:na  % For each row in the column
+        for j = 1:us.seq.numPulse  % For each row in the column
             vReconInfo(j).txnum = j;
             vReconInfo(j).rcvnum = j;
         end
-        vReconInfo(na).mode = 'accumIQ_replaceIntensity'; % accum and detect
+        vReconInfo(us.seq.numPulse).mode = 'accumIQ_replaceIntensity'; % accum and detect
     else
         vReconInfo(1).mode = 'replaceIntensity';
     end
@@ -211,7 +222,7 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
     display_image_process.method = 'imageDisplay';
     display_image_process.Parameters = {'imgbufnum',1,...   % number of buffer to process.
                          'framenum',-1,...   % (-1 => lastFrame)
-                         'pdatanum',1,...    % number of PData structure to use
+                         'pdatanum',1,...    % number of PData structure to use v%TODO: handle in linking stage.
                          'pgain',1.0,...            % pgain is image processing gain
                          'reject',2,...      % reject level
                          'persistMethod','simple',...
@@ -224,7 +235,7 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
                          'compressFactor',40,...
                          'mappingMethod','full',...
                          'display',1,...      % display image after processing
-                         'displayWindow',1};
+                         'displayWindow',1}; %TODO: handle in linking stage.
     
     save_rf_data = VSXProcess();
     save_rf_data.classname = 'External';
@@ -236,33 +247,19 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
 
 
     %% SeqControl
-    vSeqControl = VSXSeqControl();
-    vSeqControl = repmat(vSeqControl,1,6);
-    vSeqControl = sort(copy(vSeqControl));
-    
-    vSeqControl(1) = setfield(setfield(vSeqControl(1), 'command', 'jump'), 'argument', 1);
-    vSeqControl(2) = setfield(setfield(vSeqControl(2), 'command', 'timeToNextAcq'), 'argument', 160);
-    vSeqControl(3) = setfield(setfield(vSeqControl(3), 'command', 'timeToNextAcq'), 'argument', 19040);
-    vSeqControl(4) = setfield(vSeqControl(4), 'command', 'returnToMatlab');
-    vSeqControl(5) = setfield(vSeqControl(5), 'command', 'transferToHost');
-    vSeqControl(6) = setfield(setfield(vSeqControl(6), 'command', 'noop'), 'argument', 100/0.2);% 'condition', 'Hw&Sw');
-    
-    jump_to_image_start = vSeqControl(1);
-    wait_for_tx_pulse = vSeqControl(2);
-    wait_for_pulse_sequence = vSeqControl(3);
-    return_to_matlab = vSeqControl(4);
-    transfer_to_host = vSeqControl(5);
-    no_operation = vSeqControl(6);
-    
-    
-    
+    jump_to_image_start      = VSXSeqControl('command', 'jump', 'argument', 1);
+    wait_for_tx_pulse        = VSXSeqControl('command', 'timeToNextAcq', 'argument', 160);
+    wait_for_pulse_sequence  = VSXSeqControl('command', 'timeToNextAcq', 'argument', 19040);
+    return_to_matlab         = VSXSeqControl('command', 'returnToMatlab');
+    transfer_to_host         = VSXSeqControl('command', 'transferToHost');
+    no_operation             = VSXSeqControl('command', 'noop', 'argument', 100/0.2); % 'condition', 'Hw&Sw');
 
     %% Event
 
     % loop through all events
     % ---------- Events ------------- %
     vEvent = copy(repmat(VSXEvent('seqControl', wait_for_tx_pulse), [1 us.seq.numPulse]));
-    vEvent = sort(vEvent);
+    % vEvent = sort(vEvent);
     
     for i = 1:us.seq.numPulse % each transmit
         vEvent(i).info = 'Full aperture.';
@@ -293,6 +290,7 @@ function [vBlock, vTrans, vPData, vTW, vTX, vRcv, vRecon, display_image_process,
     vEvent(end+1) = VSXEvent(...
         'info', 'Jump back',...
         'seqControl', [jump_to_image_start]);
+    
     % ------------ Events ------------ %
     jump_to_image_start.argument = vEvent(1);
    
