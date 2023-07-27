9d87ce850030f396b8607b0d0ea27e9c1d77007c
Thurston Brevett <tbrevett@stanford.edu>
2023-07-27 13:25:24 -0700

    RFDataStore() : File saving bug fix.

--- RFDataStore.m
+++ RFDataStore.m
@@ -1,14 +1,22 @@
 function RFDataStore(RData)
     global TOGGLE_RFDataStore;
     save_dir  = fullfile(pwd, "RF_DATA_L7_4"); % make a path relative to the current location
-    if ~exist(save_dir, 'dir'), mkdir(save_dir); end % make a directory if non existss
+    if ~exist(save_dir, 'dir'), mkdir(save_dir); end % make a directory if non exists
     if TOGGLE_RFDataStore
+        % get save filename
         disp('Saving RF data ...')
-        fnm = fullfile(save_dir, [datestr(now,'yyyymmdd_HHMMSS') '.mat']); % filename
-        save(fnm, '-v7.3', '-nocompression', 'RData'); % TODO: maybe save as .dat file?
         fileName = datestr(now,'yyyymmdd_HHMMSS') + ".mat";
-        fileNames = [fileName, "MatFiles/qups-vsx.mat", "MatFiles/qups-conf.mat"];
-        mergeMatFiles(fileNames, "MatFiles/");
+        fnm = fullfile(save_dir, fileName); % filename
+        
+        % save raw data
+        save(fnm, '-v7.3', '-nocompression', 'RData'); % TODO: maybe save as .dat file?
+
+        % append configuration data
+        vsx = load("MatFiles/qups-vsx.mat");
+        cnf = load("MatFiles/qups-conf.mat");
+        for f = string(fieldnames(cnf))', vsx.(f) = cnf.(f); end
+        save(fnm, '-append', '-struct', 'vsx');
+        
         disp("RF data saved to " + fnm);
         TOGGLE_RFDataStore = false; % unset variable saving
     end
