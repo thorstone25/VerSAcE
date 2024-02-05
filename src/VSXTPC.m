a9480cc8ad1de0b5ab62a67a698f390b8b7800a5
Thurston <tbrevett@stanford.edu>
2024-02-05 12:39:46 -0800

    Adding TPC.

---
+++ src/VSXTPC.m
@@ -0,0 +1,19 @@
+classdef VSXTPC < matlab.mixin.Copyable
+    properties
+        name string {mustBeScalarOrEmpty} % (optional) name identifier for profile
+        maxHighVoltage double {mustBeScalarOrEmpty} % (optional) max high voltage for this profile
+        highVoltageLimit double {mustBeScalarOrEmpty} % (optional) high voltage limit based on use model
+        xmitDuration double {mustBeScalarOrEmpty} % (optional) longest transmit duration (usec)
+        hv double{mustBeScalarOrEmpty} % (optional) initial High Voltage value at startup
+    end
+    methods
+        function obj = VSXTPC(kwargs)
+            arguments
+                kwargs.?VSXTPC
+            end
+            for f = string(fieldnames(kwargs))'
+                obj.(f) = kwargs.(f); 
+            end
+        end
+    end
+end
