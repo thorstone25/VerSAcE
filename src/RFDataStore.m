b406b29207aebe562bb9899d98769149b4b75861
Thurston <tbrevett@stanford.edu>
2024-02-05 12:54:21 -0800

    Removing unnecessary lines.

--- src/RFDataStore.m
+++ src/RFDataStore.m
@@ -1,24 +1,19 @@
-function RFDataStore(RData)
-    global TOGGLE_RFDataStore;
-    save_dir  = fullfile(pwd, "RF_DATA_L7_4"); % make a path relative to the current location
-    if ~exist(save_dir, 'dir'), mkdir(save_dir); end % make a directory if non exists
-    if TOGGLE_RFDataStore
-        % get save filename
-        disp('Saving RF data ...')
-        fileName = datestr(now,'yyyymmdd_HHMMSS') + ".mat";
-        fnm = fullfile(save_dir, fileName); % filename
-        
-        % save raw data
-        save(fnm, '-v7.3', '-nocompression', 'RData'); % TODO: maybe save as .dat file?
+function RFDataStore(RData, varargin)
+global TOGGLE_RFDataStore;
+global VSXOOD_SAVE_DIR;
 
-        % append configuration data
-        vsx = load("MatFiles/qups-vsx.mat");
-        cnf = load("MatFiles/qups-conf.mat");
-        for f = string(fieldnames(cnf))', vsx.(f) = cnf.(f); end
-        save(fnm, '-append', '-struct', 'vsx');
-        
-        disp("RF data saved to " + fnm);
-        TOGGLE_RFDataStore = false; % unset variable saving
-    end
-    return
+if TOGGLE_RFDataStore
+    save_dir = VSXOOD_SAVE_DIR;
+    if ~exist(save_dir, 'dir'), mkdir(save_dir); end % make a directory if non exists
+    
+    % get save filename
+    fileName = string(datetime('now'),'yyMMdd_HHmmss_SSSS') + ".mat";
+    fnm = fullfile(save_dir, fileName); % filename
+    
+    % save raw data
+    save(fnm, '-v7.3', '-nocompression', 'RData'); % TODO: maybe save as .dat file?
+    
+    disp("RF data saved to " + fnm);
+    TOGGLE_RFDataStore = false;
+end
 end
