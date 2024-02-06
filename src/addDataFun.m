e1c8d7d454928a9255376e4de530e87414d045b3
Thurston <tbrevett@stanford.edu>
2024-02-05 21:22:57 -0800

    Adding process addition functions.

---
+++ src/addDataFun.m
@@ -0,0 +1,31 @@
+function [vUI, vEvent] = addDataFun(fnm, vRcvBuf, vSeq, kwargs)
+arguments
+    fnm (1,1) string % = "RFDataStore"
+    vRcvBuf (1,1) VSXRcvBuffer
+    vSeq (1,:) VSXSeqControl = VSXSeqControl('command','returnToMatlab')
+    kwargs.name (1,1) string = "Do "+func2str(fnm);
+    kwargs.UItyp (1,1) string {mustBeMember(kwargs.UItyp,["VsToggleButton", "VsPushButton", "None"])} = "None"
+    kwargs.UIpos (1,1) string {mustBeMember(kwargs.UIpos, ["UserB1","UserB2","UserB3","UserB4","UserB5","UserB6","UserB7","UserB8","UserC1","UserC2","UserC3","UserC4","UserC5","UserC6","UserC7","UserC8","UserA1","UserA2"])} = "UserB1";
+end
+
+% save all RF Data
+vEvent = VSXEvent('info', kwargs.name, 'seqControl', vSeq, 'process', ... 
+    VSXProcess('classname', 'External', 'method', fnm, 'Parameters',{ ...
+    'srcbuffer','receive',...
+    'srcbufnum', vRcvBuf,... 
+    'srcframenum', 0,...
+    'dstbuffer','none' ...
+})); % Process: saving data
+
+% optionally add UI to toggle
+switch kwargs.UItyp
+    case {"VsToggleButton", "VsPushButton"}
+        vUI = VSXUI( ...
+            'Control', {char(kwargs.UIpos),'Style',char(kwargs.UItyp),'Label', char(kwargs.name)}, ...
+            'Callback', cellstr(["do"+fnm+"(src, event),","global TOGGLE_"+fnm+"; TOGGLE_"+fnm+" = logical(hObj.Value);", "return;"]) ...
+            );
+    case "None", vUI = VSXUI.empty;
+    otherwise
+        error("VSXOOD:addDataFun:buttonTypeNotSupported", "Button not supported.");
+end
+
