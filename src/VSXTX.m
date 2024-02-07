465bf5bc1ed3811100204c78d8792fa1c6e647c6
Thurston Brevett <tbrevett@stanford.edu>
2024-02-07 12:40:47 -0800

    VSXTX() : default focus and Steer set to 0 instead of nan to avoid being deleted and overwritten by VSX.

--- src/VSXTX.m
+++ src/VSXTX.m
@@ -4,12 +4,12 @@ classdef VSXTX < matlab.mixin.Copyable
         Origin (1,3) double = 0
         aperture double {mustBeScalarOrEmpty, mustBeInteger, mustBePositive} = []
         Apod (1,:) double  = []
-        focus (1,1) double = nan
-        Steer (1,2) double = nan
+        focus (1,1) double = 0
+        Steer (1,2) double = 0
         FocalPt (1,3) double = nan
         % FocalPtMm (1,3) double
         Delay (1,:) double = []
-        TXPD (:,:,:,:) uint16 = []
+        TXPD (:,:,:,:) uint16 = [] % size(PData) x 3
         peakCutOff double {mustBeScalarOrEmpty} = 1.0 % [] % defaults from showTXPD
         peakBLMax  double {mustBeScalarOrEmpty} = 4.0 % [] % defaults from showTXPD
         %         VDASApod (1,:) double
