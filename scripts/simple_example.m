8d8ec776b3ba94d76620e6f4ad68fd862a62b467
Thurston <tbrevett@stanford.edu>
2024-04-19 19:18:58 -0700

    Adding simple_example.m

---
+++ scripts/simple_example.m
@@ -0,0 +1,36 @@
+%% Construct the system description
+c0 = 1500; 
+[xdc, Trans] = TransducerVerasonics("P4-2v");
+seq = SequenceRadial('type', 'PW', 'angles', -25 : 2.5 : 25, 'c0', c0);
+scn = ScanCartesian('x', 70e-3 * [-1 1]./2, 'z', [0, 100e-3]);
+us = UltrasoundSystem('xdc', xdc, 'seq', seq, 'scan', scn);
+[scn.dx, scn.dz] = deal(us.lambda / 4);
+
+%% Create necessary VSX objects (outside of QUPS)
+vRes = VSXResource(); % global resource definition
+vTW  = VSXTW('type','parametric', 'Parameters', [Trans.frequency, 0.67, 2, 1]); % tx waveform
+vTPC = VSXTPC('name','Default', 'hv', Trans.maxHighVoltage); % max power
+
+% Create a VSXBlock
+vb = QUPS2VSX(us, Trans, vRes, 'vTW', vTW, 'vTPC', vTPC, 'recon_VSX', true);
+
+% Create the structs
+vs = link(vb, vRes, Trans, 'TXPD', true); % link
+pt1; vs.Media = Media; % add simulation media
+
+% Save
+filename = 'MatFiles/qups-vsx.mat';
+save(filename, '-struct', 'vs');
+
+%% Run
+VSX;
+
+%% Post-process
+if ~exist('RcvData', 'var'), RcvData = {RData}; end
+c0   = Resource.Parameters.speedOfSound;
+[us, chd] = UltrasoundSystem.Verasonics(Trans, TX, TW, 'c0', c0, 'Receive', Receive, 'RcvData', RcvData); % import to QUPS                                [us, chd] = UltrasoundSystem.Verasonics(Trans, TX, TW, 'Receive', Receive, 'RcvData', RcvData); % import to QUPS
+sct = Scatterers.Verasonics(Media, 'c0', c0, 'scale', c0./us.xdc.fc);
+
+chd = hilbert(singleT(chd));
+figure; plot(us); hold on; plot(sct, '.');
+figure; imagesc(chd); dbr echo 80; animate(chd.data, 'loop', false);
